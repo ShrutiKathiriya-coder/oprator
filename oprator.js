fdfff3b35f2e3557725a1f3eb0ab9103c567a1c8
ShrutiKathiriya-coder <kathiriyashruti3@gmail.com>
2024-11-21 18:31:40 +0530

    Add files via upload

---
+++ oprator.js
@@ -0,0 +1,71 @@
+// oprator
+
+// a+b => (+) = optator and a & b = oprand => (a+b)=>expresion
+
+// 1) arthmatic oprator
+// sign => + - / % ++ -- ** 
+
+let a =10,b=5;
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a*b);
+ console.log(a/b);
+ console.log(a%b);
+ console.log(a++) //postfix 
+ console.log(a);
+ console.log(++a); //prifix
+ console.log(a);
+ 
+
+//  2) condition oprator
+// sign=> < > <= >= == === != !== 
+
+// == => only check the value   or !=
+ // === =>check the value and data type  or !== 
+ 
+ let x=10,y=5;
+
+console.log(x<y);
+console.log(x>y);
+console.log(x<=y);
+console.log(x>=y);
+console.log(x==y);
+console.log(x!=y);
+
+// 3) assigement oprator (short hand proprty)
+
+let s=10,i=5;
+console.log(s+=y);
+console.log(s-=i);
+console.log(s*=i);
+console.log(s/=i);
+console.log(s%=i);
+
+//4) logical oprator
+
+//&& || !
+
+let c=0,d=1;
+console.log(c&&d);
+console.log(c||d);
+
+//5) typeof oprator
+
+// any variable check of in value
+let e=10,f="5";
+    console.log(typeof e);
+    console.log(typeof f);
+    
+    
+
+
+
+
+
+
+
+
+
+
+ 
+ 
